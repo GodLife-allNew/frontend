da2786d4c9e6d29c2b55a6f27892ef1d2b53b06d
rimmmo <limcl1224@naver.com>
2025-10-25 20:09:15 +0900

    25/10/25 (chaerim) Modify: 컴포넌트명 수정

--- src/app/router/featureRouters/noticeRoutes.js
+++ src/app/router/featureRouters/noticeRoutes.js
@@ -1,10 +1,10 @@
 import NoticeListPage from "@/pages/Notice/NoticeList";
 import NoticeDetail from "@/pages/Notice/NoticeDetail";
 
-import { Edit, Detail, List } from "@/features/notice";
+import { Edit } from "@/features/notice";
 
 export const NOTICE_ROUTES = [
-  { path: "/notice/detail/:noticeIdx", element: <Detail /> },
+  { path: "/notice/detail/:noticeIdx", element: <NoticeDetail /> },
   { path: "/notice/edit/:noticeIdx", element: <Edit /> },
   { path: "/notice/create", element: <Edit /> },
   { path: "/notice/list", element: <NoticeListPage /> },
