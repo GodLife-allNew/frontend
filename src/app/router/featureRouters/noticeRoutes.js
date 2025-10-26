da2857ceaf25c6254134f3266243815877569706
rimmmo <limcl1224@naver.com>
2025-10-26 22:25:39 +0900

    25/10/26 (chaerim) Modify : import 코드 수정

--- src/app/router/featureRouters/noticeRoutes.js
+++ src/app/router/featureRouters/noticeRoutes.js
@@ -1,15 +1,12 @@
-import NoticeList from "@/features/notice/pages/NoticeList";
-import NoticeDetail from "@/features/notice/pages/NoticeDetail";
-
-import { Edit } from "@/features/notice";
+import { List, Detail, Edit } from "@/features/notice";
 
 export const NOTICE_ROUTES = [
-  { path: "/notice/detail/:noticeIdx", element: <NoticeDetail /> },
+  { path: "/notice/detail/:noticeIdx", element: <Detail /> },
   { path: "/notice/edit/:noticeIdx", element: <Edit /> },
   { path: "/notice/create", element: <Edit /> },
-  { path: "/notice/list", element: <NoticeList /> },
+  { path: "/notice/list", element: <List /> },
 
   // 관리자용 라우트 (새로 추가)
-  { path: "/admin/notice/detail/:noticeIdx", element: <NoticeDetail isAdminMode={true} /> },
+  { path: "/admin/notice/detail/:noticeIdx", element: <Detail isAdminMode={true} /> },
   { path: "/admin/notice/edit/:noticeIdx", element: <Edit isAdminMode={true} /> },
 ];
