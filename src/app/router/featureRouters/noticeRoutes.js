9f99e64949b5c63860f4c7f4ec589a65086c6024
rimmmo <limcl1224@naver.com>
2025-10-26 22:53:42 +0900

    25/10/26 (chaerim) Modify: 경로 수정

--- src/app/router/featureRouters/noticeRoutes.js
+++ src/app/router/featureRouters/noticeRoutes.js
@@ -2,11 +2,10 @@ import { List, Detail, Edit } from "@/features/notice";
 
 export const NOTICE_ROUTES = [
   { path: "/notice/detail/:noticeIdx", element: <Detail /> },
-  { path: "/notice/edit/:noticeIdx", element: <Edit /> },
-  { path: "/notice/create", element: <Edit /> },
   { path: "/notice/list", element: <List /> },
 
   // 관리자용 라우트 (새로 추가)
   { path: "/admin/notice/detail/:noticeIdx", element: <Detail isAdminMode={true} /> },
-  { path: "/admin/notice/edit/:noticeIdx", element: <Edit isAdminMode={true} /> },
+  { path: "/notice/edit/:noticeIdx", element: <Edit isAdminMode={true} /> },
+  { path: "/notice/create", element: <Edit isAdminMode={true} /> },
 ];
